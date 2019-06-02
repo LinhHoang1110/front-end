330182e0b358cafe9d33e9df920e691630936895
thundergod3 <congthundergodphan@gmail.com>
2019-06-02 15:02:15 +0700

    'update'

--- src/constants/Config.js
+++ src/constants/Config.js
@@ -1,2 +1,2 @@
-export const API_URL = "http://10.1.9.121:6969"
+export const API_URL = process.env.NODE_ENV  === "development" ? "http://10.1.9.121:6969" : "https://back-end-lc-commerce.herokuapp.com"
 // export const API_URL = "http://localhost:6969"
